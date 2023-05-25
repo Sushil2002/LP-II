aca2c49f39643651717a272a9e1387eb5540ac51
Sushilkumar Dhamane <103381144+Sushil2002@users.noreply.github.com>
2023-05-25 23:51:38 +0530

    Update Astar.cpp

--- Assignment02/Astar.cpp
+++ Assignment02/Astar.cpp
@@ -1,62 +1,65 @@
 #include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 int main()
 {
-    int n, edges;
-    cin >> n >> edges;
-    vector<pair<int, int>> graph[n];
-    for (int i = 0; i < edges; i++)
+    int n;
+    cout << "Enter the Number of Vertex - ";
+    cin >> n;
+    int e;
+    cout << "Enter the Number of Edges - ";
+    cin >> e;
+    vector<pair<int, int>> adj[n]; //{ node,wt}
+    for (int i = 0; i < e; i++)
     {
-        int a, b, w;
-        cout << "enter src dest weight" << endl;
-        cin >> a >> b >> w;
-        graph[a].push_back({b, w});
-        graph[b].push_back({a, w});
+        int u, v, w;
+        cout << "\nEnter src,dest and wt - ";
+        cin >> u >> v >> w;
+        adj[u].push_back({v, w});
+        adj[v].push_back({u, w});
     }
-    int hr[n];
-    int f[n];
+    int h[n];
     int g[n];
-    cout << "enter the heuristic value one by one-" << n << endl;
+    int f[n];
+    cout << "\nEnter the Huristic Value - ";
     for (int i = 0; i < n; i++)
     {
-        cin >> hr[i];
+        cin >> h[i];
     }
-
     g[0] = 0;
-    f[0] = g[0] + hr[0];
+    f[0] = g[0] + h[0];
     for (int i = 1; i < n; i++)
     {
         g[i] = INT_MAX;
     }
-    vector<int> pr(n, -1);
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-    pq.push({f[0], 0});//f(n),wt
-    while (!pq.empty())
+    vector<int> par(n, -1);
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> q;
+    q.push({f[0], 0}); // f(n),node
+    while (!q.empty())
     {
-        auto front_pair = pq.top();
-        int parent = front_pair.second;
-        pq.pop();
-        for (auto c : graph[parent])
+        auto it = q.top();
+        int parent = it.second;
+        q.pop();
+        for (auto i : adj[parent])
         {
-            int child = c.first;
-            int weight = c.second;
+            int child = i.first;
+            int weight = i.second;
             int dist = g[parent] + weight;
             if (dist < g[child])
             {
                 g[child] = dist;
-                f[child] = dist + hr[child];
-                pq.push({f[child], child});
-                pr[child] = parent;
+                f[child] = g[child] + h[child];
+                q.push({f[child], child});
+                par[child] = parent;
             }
         }
     }
-    int dest = n - 1;
-    int i = dest;
+    int i = n - 1;
     vector<int> path;
     while (i != -1)
     {
         path.push_back(i);
-        i = pr[i];
+        i = par[i];
     }
     reverse(path.begin(), path.end());
     cout << "path is" << endl;
@@ -64,4 +67,5 @@ int main()
     {
         cout << c << "-->";
     }
+    return 0;
 }
